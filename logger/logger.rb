86bbf9b95e075f2029a2abaa5940ae968319a625
kcz <1732074+cookie-s@users.noreply.github.com>
2022-08-27 10:48:20 +0900

    logger: fix
    Co-authored-by: Koki Takahashi <hakatasiloving@gmail.com>

--- logger/logger.rb
+++ logger/logger.rb
@@ -6,5 +6,5 @@ require './lib/slack_rtm_receiver'
 
 config = YAML.load_file('./config.yml')
 
-receiver = Slack::Events.config.signing_secret ? SlackEventsReceiver.new(config['slack']['team_id']) : SlackRTMReceiver.new
+receiver = config['slack']['event_api'] ? SlackEventsReceiver.new(config['slack']['team_id']) : SlackRTMReceiver.new
 SlackLogger.new.start receiver
