5752de809741ab9899e86ac43298a9e16a1f5784
cookie-s <kcz@kcz.sh>
2022-08-29 20:20:13 -0400

    logger: loop than recursion

--- lib/collector/rtm.rb
+++ lib/collector/rtm.rb
@@ -1,45 +1,46 @@
 module Collector
   class RTM
     def start!(logger)
-      realtime = Slack::RealTime::Client.new
+      loop do
+        realtime = Slack::RealTime::Client.new
 
-      realtime.on :message do |m|
-        puts 'new message'
-        logger.new_message(m)
+        realtime.on :message do |m|
+          puts 'new message'
+          logger.new_message(m)
+        end
+
+        realtime.on :team_join do |e|
+          puts "new user has joined"
+          logger.update_users
+        end
+
+        realtime.on :user_change do |e|
+          puts "user data has changed"
+          logger.update_users
+        end
+
+        realtime.on :channel_created do |c|
+          puts "channel has created"
+          logger.update_channels
+        end
+
+        realtime.on :channel_rename do |c|
+          puts "channel has renamed"
+          logger.update_channels
+        end
+
+        realtime.on :emoji_changed do |c|
+          puts "emoji has changed"
+          logger.update_emojis
+        end
+
+        # if connection closed, restart the realtime logger
+        realtime.on :close do
+          puts "websocket disconnected"
+        end
+
+        realtime.start!
       end
-
-      realtime.on :team_join do |e|
-        puts "new user has joined"
-        logger.update_users
-      end
-
-      realtime.on :user_change do |e|
-        puts "user data has changed"
-        logger.update_users
-      end
-
-      realtime.on :channel_created do |c|
-        puts "channel has created"
-        logger.update_channels
-      end
-
-      realtime.on :channel_rename do |c|
-        puts "channel has renamed"
-        logger.update_channels
-      end
-
-      realtime.on :emoji_changed do |c|
-        puts "emoji has changed"
-        logger.update_emojis
-      end
-
-      # if connection closed, restart the realtime logger
-      realtime.on :close do
-        puts "websocket disconnected"
-        start! logger
-      end
-
-      realtime.start!
     end
   end
 end
