349bea274aa4563981f38658e370caf03632522b
Koki Takahashi <hakatasiloving@gmail.com>
2023-02-08 11:22:03 +0000

    Configure default values for display options

--- viewer/front/reducers/displayOptions.js
+++ viewer/front/reducers/displayOptions.js
@@ -1,6 +1,11 @@
 import SlackConstants from '../constants/SlackConstants';
 
-const displayOptions = (state = Object.create(null), action) => {
+const DEFAULT_OPTIONS = {
+  hideBotMessages: false,
+  hideThreadOnlyMessages: true,
+};
+
+const displayOptions = (state = DEFAULT_OPTIONS, action) => {
   switch (action.type) {
     case SlackConstants.UPDATE_DISPLAY_OPTION:
       return {
