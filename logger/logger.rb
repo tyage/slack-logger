27b3c1dd4cdfbe5969cf08b2e1a504c36f99ad87
cookie-s <kcz@kcz.sh>
2023-04-28 00:37:19 -0400

    module SlackPatron

--- logger/logger.rb
+++ logger/logger.rb
@@ -6,5 +6,5 @@ require './lib/collector/rtm'
 
 config = YAML.load_file('./config.yml')
 
-collector = config['slack']['use_events_api'] ? Collector::EventsAPI.new(config['slack']['team_id']) : Collector::RTM.new
+collector = config['slack']['use_events_api'] ? SlackPatron::Collector::EventsAPI.new(config['slack']['team_id']) : SlackPatron::Collector::RTM.new
 SlackLogger.new.start collector
